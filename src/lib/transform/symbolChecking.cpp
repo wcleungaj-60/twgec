f9ddda0b49de92fd04c97b218c53a3cabafbc842
wcleung <leungwingchung60@gmail.com>
2025-08-11 00:21:53 +0800

    [twgec][transform] Support define before use check

--- src/lib/transform/symbolChecking.cpp
+++ src/lib/transform/symbolChecking.cpp
@@ -1,7 +1,10 @@
 #include "ast.h"
 #include "location.h"
 #include "transform.h"
+#include <algorithm>
+#include <iterator>
 #include <memory>
+#include <set>
 namespace transform {
 
 using std::string;
@@ -13,11 +16,10 @@ bool throwRedefinitionError(std::string name, Location loc1, Location loc2) {
   return false;
 }
 
-bool redefinitionChecking(
-    const unique_ptr<ModuleNode> &moduleNode,
-    std::map<std::string, std::unique_ptr<ConstDefNode>> &constDefMap,
-    std::map<std::string, std::unique_ptr<FunDefNode>> &funDefMap,
-    std::map<std::string, std::unique_ptr<BlockNode>> &blockMap) {
+bool redefinitionChecking(const unique_ptr<ModuleNode> &moduleNode) {
+  std::map<std::string, std::unique_ptr<ConstDefNode>> constDefMap;
+  std::map<std::string, std::unique_ptr<FunDefNode>> funDefMap;
+  std::map<std::string, std::unique_ptr<BlockNode>> blockMap;
   for (auto &constDef : moduleNode->constDefs) {
     if (constDefMap.count(constDef->key) != 0)
       return throwRedefinitionError(
@@ -51,12 +53,53 @@ bool redefinitionChecking(
   return true;
 }
 
+bool hasUnitializedFun(std::unique_ptr<InstrSetNode> &instrSet,
+                       std::set<std::string> uninitializedFunDef) {
+  bool ret = false;
+  for (auto &compositeInstr : instrSet->instructions) {
+    if (compositeInstr->instruction)
+      if (uninitializedFunDef.count(compositeInstr->instruction->identifier)) {
+        std::cerr << "Compilation Error: function `"
+                  << compositeInstr->instruction->identifier
+                  << "` is defined before used at "
+                  << compositeInstr->instruction->loc << ".\n";
+        ret = true;
+      }
+    if (compositeInstr->ifStatement)
+      if (hasUnitializedFun(compositeInstr->ifStatement->trueBlock,
+                            uninitializedFunDef))
+        ret = true;
+  }
+  return ret;
+}
+
+bool useBeforeDefineChecking(const unique_ptr<ModuleNode> &moduleNode) {
+  bool ret = true;
+  std::set<std::string> allFunDef;
+  for (auto &funDef : moduleNode->funDefs)
+    allFunDef.insert(funDef->identifier);
+  std::set<std::string> definedFunDef;
+  for (auto &funDef : moduleNode->funDefs) {
+    std::set<std::string> uninitializedFunDef;
+    std::set_difference(
+        allFunDef.begin(), allFunDef.end(), definedFunDef.begin(),
+        definedFunDef.end(),
+        std::inserter(uninitializedFunDef, uninitializedFunDef.begin()));
+    if (funDef->blockBody)
+      for (auto &typedInstrSet : funDef->blockBody->typedInstrSets)
+        if (hasUnitializedFun(typedInstrSet->instrSet, uninitializedFunDef))
+          ret = false;
+    if (funDef->typedInstrSet)
+      if (hasUnitializedFun(funDef->typedInstrSet->instrSet,
+                            uninitializedFunDef))
+        ret = false;
+    definedFunDef.insert(funDef->identifier);
+  }
+  return ret;
+}
+
 bool symbolChecking(const unique_ptr<ModuleNode> &moduleNode) {
-  std::map<std::string, std::unique_ptr<ConstDefNode>> constDefMap;
-  std::map<std::string, std::unique_ptr<FunDefNode>> funDefMap;
-  std::map<std::string, std::unique_ptr<BlockNode>> blockMap;
-  if (!redefinitionChecking(moduleNode, constDefMap, funDefMap, blockMap))
-    return false;
-  return true;
+  return redefinitionChecking(moduleNode) &&
+         useBeforeDefineChecking(moduleNode);
 }
 } // namespace transform
