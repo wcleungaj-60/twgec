fb12ce15a91398ea7ce5f7703caeed6c700621d5
wcleung <leungwingchung60@gmail.com>
2025-10-08 23:41:09 +0800

    [twgec][example] Fix simple-rpg load/store order
    - added `update-example` script

---
+++ example/update-example.sh
@@ -0,0 +1,3 @@
+twgec example/royalVsSkydow/test.twge -o=example/royalVsSkydow/output.events
+twgec example/simple-rpg/test.twge -o=example/simple-rpg/output.events
+twgec example/simple-rpg/test.twge --print-ast-after-only=implicitListPromotion > example/simple-rpg/final.twge
